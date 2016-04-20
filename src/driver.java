3f2634761e3693e65712d95451948f297983bb52
Yue Teng <itengyue@gmail.com>
2016-04-19 20:26:59 -0400

    "bfs add argument threshold"

--- src/driver.java
+++ src/driver.java
@@ -3,7 +3,7 @@ public class driver {
 	public static void main(String[] args) {
 		StlReader sr = new StlReader();
 		sr.read("tri.stl");
-		sr.bfs();
+		sr.bfs(Math.PI * 0.25);
 		System.out.println(sr.groupTable.values());
 		System.out.println(sr.vertexIndexTable);
 		VrmlWriter.write(sr.groupTable, sr.vertexIndexTable, "test.wrl");
