982325125e1dc06c78780bfa5fc28d968506defe
Yue Teng <itengyue@gmail.com>
2016-04-19 22:13:50 -0400

    "\\s+"

--- src/driver.java
+++ src/driver.java
@@ -2,7 +2,7 @@
 public class driver {
 	public static void main(String[] args) {
 		StlReader sr = new StlReader();
-		sr.read("tri.stl");
+		sr.read("cube1.stl");
 		sr.bfs(Math.PI * 0.25);
 		System.out.println(sr.groupTable.values());
 		System.out.println(sr.vertexIndexTable);
