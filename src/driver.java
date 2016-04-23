358eba3adda3b0d566caa54162aee049bdd0c587
Jackaback <hdu1@andrew.cmu.edu>
2016-04-23 15:23:59 -0400

    .

--- src/driver.java
+++ src/driver.java
@@ -2,10 +2,14 @@
 public class driver {
 	public static void main(String[] args) {
 		StlReader sr = new StlReader();
-		sr.read("c172r.stl");
+		sr.read("magnolia.stl");
 		sr.bfs(Math.PI * 0.25);
-		System.out.println(sr.groupTable.values());
-		System.out.println(sr.vertexIndexTable);
+//		for (Polygon p : sr.groupTable.keySet()) {
+//			System.out.println(p);
+//			System.out.println(sr.groupTable.get(p));
+//		}
+		System.out.println(sr.groupTable.keySet().size());
+		//System.out.println(sr.vertexIndexTable);
 		VrmlWriter.write(sr.groupTable, sr.vertexList, sr.vertexIndexTable, "test.wrl");
 	}
 }
